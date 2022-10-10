66a202b34982a52683cc8b0617713148558c7ec3
mr.agape <32167063+SHODIBEK@users.noreply.github.com>
2022-10-10 16:04:54 +0500

    add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,9 @@
 import './vendor';
 import './helpers';
-import './components/social';
 import {ieFix} from './vendor/ie-fix';
 import {vhFix} from './vendor/vh-fix';
 import {actualYear} from './modules/actualYear';
 import header from './components/header';
-import lazyLoading from './modules/lazyLoading';
 import scrollToAnchor from './modules/scrollToAnchor';
 
 ieFix();
@@ -14,4 +12,3 @@ actualYear();
 scrollToAnchor.init();
 
 header.init();
-lazyLoading.init();
